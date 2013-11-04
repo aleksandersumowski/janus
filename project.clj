83d2a40c759ae8a447a69e4500995de420407cff
Aleksander Sumowski <asumowsk@thoughtworks.com>
2013-11-05 00:01:16 +0530

    leinignen 2

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject janus "0.0.2"
   :description "Consumer-driven contracts, verified both ways."
   :url "http://github.com/gga/janus"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.3"]
                  [json-path "0.3.0"]
                  [clj-http "0.7.7"]
-		     [lein-idea "1.0.1"]
+                 [lein-idea "1.0.1"]
                  [org.clojure/core.logic "0.8.4"]
                  [org.clojure/tools.cli "0.2.4"]
                  [org.clojure/data.xml "0.0.7"]]
-  :dev-dependencies [ [clj-http-fake "0.4.1"]
-                     [midje "1.5.1"]
-                     [lein-midje "3.1.1"]]
+  :profiles {:dev {:dependencies [[clj-http-fake "0.4.1"]
+                                  [midje "1.5.1"]
+                                  [lein-midje "3.1.1"][clj-stacktrace "0.2.4"]]}}
   :main janus)
